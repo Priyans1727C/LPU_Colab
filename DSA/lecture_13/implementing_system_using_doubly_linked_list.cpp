fbf1be834247e39e69882ef6510821c77ea2838c
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-09-26 22:08:28 +0530

    Create implementing_system_using_doubly_linked_list.cpp
    ﻿You are tasked with implementing a system that maintains a record of student scores in a course. The system should allow for the insertion of scores at the front of a doubly linked list and display the updated list after each insertion.
    
    
    
    Note: This is a sample question asked in a Capgemini interview.
    
    Input format :
    The input consists of a series of integer scores to be inserted into the list, one score per line.
    
    Output format :
    After each score insertion, the program should display the updated list of scores.
    
    The scores should be displayed in the order they were inserted, from newest to oldest.
    
    Sample test cases :
    Input 1 :
    80
    75
    90
    Output 1 :
    Node Inserted
    80 
    Node Inserted
    75 80 
    Node Inserted
    90 75 80 
    Input 2 :
    95
    88
    72
    99
    Output 2 :
    Node Inserted
    95 
    Node Inserted
    88 95 
    Node Inserted
    72 88 95 
    Node Inserted
    99 72 88 95

---
+++ DSA/lecture_13/implementing_system_using_doubly_linked_list.cpp
@@ -0,0 +1,56 @@
+// You are using GCC
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) : data(val), next(nullptr), prev(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void insertAtFront(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList scoreList;
+
+    int score;
+    while (cin >> score) {
+        scoreList.insertAtFront(score);
+        cout << "Node Inserted" << endl;
+        scoreList.display();
+    }
+
+    return 0;
+}
