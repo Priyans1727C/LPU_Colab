6b89693bfdd374ecf97d9b4e51b9fa40ddcf0028
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-09-29 12:54:04 +0530

    Create student_attendance_system.cpp
    ﻿You are developing a student attendance system for a school. As part of the system, you need to implement a functionality that counts the number of occurrences of a specific student's ID in the attendance queue. The attendance queue is implemented using an array-based queue.
    
    
    
    The attendance system should adhere to the following requirements:
    
    Initialize Queue: The system should initialize the attendance queue to an empty state.
    Enqueue Student ID: As students enter the classroom, their ID (represented by integers) should be enqueued into the attendance queue.
    Count Occurrences: Given a specific student ID, the system should count the number of times the ID appears in the attendance queue.
    Input format :
    The first line of input consists of an integer N, representing the number of students' IDs to enqueue.
    
    The second line consists of N space-separated integers, each representing a student's ID to enqueue.
    
    The third line consists of an integer representing the student ID for which the occurrences need to be counted.
    
    Output format :
    The output prints the count of occurrences of the given ID in the attendance queue.
    
    
    
    Refer to the sample output for the exact text and format.
    
    Code constraints :
    The student ID is a positive integer.
    
    The maximum number of students is limited by the MAX_SIZE constant, which is set to 100.
    
    Sample test cases :
    Input 1 :
    6
    1 2 2 3 2 4
    2
    Output 1 :
    Occurrences of 2 in the queue: 3
    Input 2 :
    5
    10 10 10 10 10
    5
    Output 2 :
    Occurrences of 5 in the queue: 0

---
+++ DSA/lecture_17/student_attendance_system.cpp
@@ -0,0 +1,32 @@
+// You are using GCC
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+
+    int fq;
+    cin>>fq;
+
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==fq){
+            count++;
+        }
+    }
+
+    cout<<"Occurrences of "<<fq<<" in the queue: "<<count;
+
+
+
+    return 0;
+}
