f21526b18a980f2c487a21f8a5525b16a5f3365c
Priyanshu Singh <117721542+Priyans1727C@users.noreply.github.com>
2023-11-04 20:46:00 +0530

    Create Mark_Antony.cpp
    The group of archaeologists studying ancient artifacts has presented Mark Antony with a perplexing challenge. They've come across an enigmatic array of elements that appear to depict a min-heap tree, but they are struggling to efficiently extract the largest element from this min-heap.
    
    
    
    Mark Antony's task is to assist them in transforming the provided min-heap array into a max-heap representation. This conversion will facilitate the straightforward retrieval of the largest element from the tree.
    
    
    
    Note: This kind of question will be helpful in clearing Accenture recruitment.
    
    Input format :
    The first line of input consists of an integer n, representing the number of elements in the min heap tree.
    
    The second line of input consists of n space-separated integer elements, forming the min heap sequence.
    
    Output format :
    The output prints the space-separated n integer values of the max heap array formed after the conversion.
    
    
    
    Refer to the sample output for further format specifications.
    
    Code constraints :
    The test cases will fall under the following constraints:
    
    1 < n < 30
    
    1 < array elements < 35
    
    Sample test cases :
    Input 1 :
    10
    3 5 9 6 8 20 10 12 18 9
    Output 1 :
    20 18 10 12 9 9 3 5 6 8 
    Input 2 :
    5
    5 6 8 11 15
    Output 2 :
    15 11 8 5 6

---
+++ DSA/lecture_32/Mark_Antony.cpp
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+void MaxHeapify(int arr[], int i, int N) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < N && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < N && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+        MaxHeapify(arr, largest, N);
+    }
+}
+
+void convertMaxHeap(int arr[], int N) {
+    for (int i = (N - 2) / 2; i >= 0; --i)
+        MaxHeapify(arr, i, N);
+}
+
+void printArray(int* arr, int size) {
+    for (int i = 0; i < size; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arrayCount;
+    scanf("%d", &arrayCount);
+    int arr[arrayCount];
+    for (int i = 0; i < arrayCount; i++) {
+        scanf("%d", &arr[i]);
+    }
+    convertMaxHeap(arr, arrayCount);
+    printArray(arr, arrayCount);
+    return 0;
+}
